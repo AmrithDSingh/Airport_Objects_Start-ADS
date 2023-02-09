2b01a441887154be0e2ac3557a222c541b39a08f
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-02-09 11:11:16 +0000

    practicing classes

--- Bag.js
+++ Bag.js
@@ -1,4 +1,9 @@
-class Bag {
+class Bag { constructor(weight){
+    if(!weight){
+        throw new console.error("Bag must have a weight");
+    }
+    this.weight = weight; 
+}
     // Include constructor Method with the following property: 
         // 1. Weight
             //1a. Should handle an exception in case user adds no weight and throw an error.
@@ -6,3 +11,4 @@ class Bag {
 }
 
 //Export the module
+module.exports = Bag
